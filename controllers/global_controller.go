4bfdb2640fd50ce4cf7ca910dd614244a3071339
nicholasrussel <nicholasrussel1234@gmail.com>
2022-04-02 14:19:47 +0700

    add-controller-user-insert and update

--- controllers/global_controller.go
+++ controllers/global_controller.go
@@ -48,3 +48,70 @@ func Logout(w http.ResponseWriter, r *http.Request) {
 	ResetUserToken(w)
 	SendSuccessResponse(w)
 }
+func InsertUsers(w http.ResponseWriter, r *http.Request) {
+
+	// connect to database
+	db := Connect()
+	defer db.Close()
+
+	err := r.ParseForm()
+	if err != nil {
+		return
+	}
+	fullname := r.Form.Get("fullname")
+	username := r.Form.Get("username")
+	email := r.Form.Get("email")
+	password := r.Form.Get("password")
+	address := r.Form.Get("address")
+	user_type := r.Form.Get("user_type")
+	partner_type := r.Form.Get("partner_type")
+	company_name := r.Form.Get("company_name")
+	date_created := r.Form.Get("date_created")
+
+	_, errQuery := db.Exec("INSERT INTO users(fullname,username,email,password,address,user_type,partner_type,company_name,date_created) values (?,?,?,?,?,?,?,?,?)", fullname, username, email, password, address, user_type, partner_type, company_name, date_created)
+
+	if errQuery == nil {
+		SendSuccessResponse(w)
+	} else {
+		SendErrorResponse(w, 400)
+	}
+
+	db.Close()
+}
+func UpdateUsers(w http.ResponseWriter, r *http.Request) {
+
+	// connect to database
+	db := Connect()
+	defer db.Close()
+
+	err := r.ParseForm()
+	if err != nil {
+		return
+	}
+	fullname := r.Form.Get("fullname")
+	username := r.Form.Get("username")
+	email := r.Form.Get("email")
+	password := r.Form.Get("password")
+	address := r.Form.Get("address")
+	user_type := r.Form.Get("user_type")
+	partner_type := r.Form.Get("partner_type")
+	company_name := r.Form.Get("company_name")
+	date_created := r.Form.Get("date_created")
+
+	result, errQuery := db.Exec("UPDATE users SET fullname=?, username=?, password=?, address=?, user_type=?, partner_type=?, company_name=?, date_created=? WHERE email=?", fullname, username, password, address, user_type, partner_type, company_name, date_created, email)
+
+	num, _ := result.RowsAffected()
+
+	if errQuery == nil {
+		if num == 0 {
+			SendErrorResponse(w, 400)
+		} else {
+			SendSuccessResponse(w)
+			log.Println(email)
+		}
+	} else {
+		SendErrorResponse(w, 400)
+	}
+
+	db.Close()
+}
