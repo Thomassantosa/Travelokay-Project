2fa24f64d42a744bd398512eb980b6e61811da06
nicholasrussel <nicholasrussel1234@gmail.com>
2022-04-03 14:32:31 +0700

    add-model-hotel,room,flight,bustrip,traintrip,response

--- models/model.go
+++ models/model.go
@@ -14,15 +14,68 @@ type User struct {
 }
 
 type Hotel struct {
-	ID            int     `json:"id"`
-	HotelName     string  `json:"hotel_name"`
-	HotelStar     int     `json:"hotel_star"`
-	HotelRating   float32 `json:"hotel_rating"`
-	HotelReview   int     `json:"hotel_review"`
-	HotelFacility string  `json:"hotel_facility"`
-	HotelAddress  string  `json:"hotel_address"`
-	HotelCity     string  `json:"hotel_city"`
-	HotelCountry  string  `json:"hotel_country"`
+	ID            int    `json:"hotel_id"`
+	HotelName     string `json:"hotel_name"`
+	HotelStar     int    `json:"hotel_star"`
+	HotelRating   int    `json:"hotel_rating"`
+	HotelReview   int    `json:"hotel_review"`
+	HotelFacility string `json:"hotel_facility"`
+	HotelAddress  string `json:"hotel_address"`
+	HotelCity     string `json:"hotel_city"`
+	HotelCountry  string `json:"hotel_country"`
+}
+
+type Room struct {
+	ID           int    `json:"room_id"`
+	HotelID      int    `json:"hotel_id"`
+	RoomName     string `json:"room_name"`
+	RoomType     string `json:"room_type"`
+	RoomPrice    int    `json:"room_price"`
+	RoomFacility string `json:"room_facility"`
+	RoomCapacity int    `json:"room_capacity"`
+	RoomStatus   int    `json:"room_status"`
+	CheckIn      string `json:"checkin"`
+	CheckOut     string `json:"checkout"`
+}
+
+type Flight struct {
+	ID                 int    `json:"flight_id"`
+	AirplaneID         int    `json:"airplane_id"`
+	DepartureAirport   int    `json:"departure_airport"`
+	DestinationAirport int    `json:"destination_airport"`
+	FlightType         string `json:"flight_type"`
+	FlightNumber       string `json:"flight_number"`
+	DepartureTime      string `json:"departure_time"`
+	DepartureDate      string `json:"departure_date"`
+	ArrivalTime        string `json:"arrival_time"`
+	ArrivalDate        string `json:"arrival_date"`
+	TravelTime         int    `json:"travel_time"`
+}
+
+type Bustrip struct {
+	ID                    int    `json:"bustrip_id"`
+	BusID                 int    `json:"bus_id"`
+	DepartureBusstation   int    `json:"departure_busstation"`
+	DestinationBusstation int    `json:"destination_busstation"`
+	BusNumber             string `json:"bus_number"`
+	DepartureTime         string `json:"departure_time"`
+	DepartureDate         string `json:"departure_date"`
+	ArrivalTime           string `json:"arrival_time"`
+	ArrivalDate           string `json:"arrival_date"`
+	TravelTime            int    `json:"travel_time"`
+}
+
+type Traintrip struct {
+	ID                 int    `json:"traintrip_id"`
+	TrainID            int    `json:"train_id"`
+	DepartureStation   int    `json:"departure_station"`
+	DestinationStation int    `json:"destination_station"`
+	TraintripNumber    string `json:"trainTrip_number"`
+	DepartureTime      string `json:"departure_time"`
+	DepartureDate      string `json:"departure_date"`
+	ArrivalTime        string `json:"arrival_time"`
+	ArrivalDate        string `json:"arrival_date"`
+	TravelTime         int    `json:"travel_time"`
 }
 
 type MessageResponse struct {
@@ -52,4 +105,51 @@ type HotelsResponse struct {
 	Status  int     `json:"status"`
 	Message string  `json:"message"`
 	Data    []Hotel `json:"data"`
+}
+
+type RoomResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Data    Room   `json:"data"`
+}
+
+type RoomsResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Data    []Room `json:"data"`
+}
+
+type FlightResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Data    Flight `json:"data"`
+}
+
+type FlightsResponse struct {
+	Status  int      `json:"status"`
+	Message string   `json:"message"`
+	Data    []Flight `json:"data"`
+}
+type BusResponse struct {
+	Status  int     `json:"status"`
+	Message string  `json:"message"`
+	Data    Bustrip `json:"data"`
+}
+
+type BusesResponse struct {
+	Status  int       `json:"status"`
+	Message string    `json:"message"`
+	Data    []Bustrip `json:"data"`
+}
+
+type TrainResponse struct {
+	Status  int       `json:"status"`
+	Message string    `json:"message"`
+	Data    Traintrip `json:"data"`
+}
+
+type TrainsResponse struct {
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+	Data    []Traintrip `json:"data"`
 }
