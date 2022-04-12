4e92f5487e4b1aded5e95108351c5ebe31533fca
nicholasrussel <nicholasrussel1234@gmail.com>
2022-04-12 13:30:59 +0700

    add-getrefundlist

--- controllers/admin_controller.go
+++ controllers/admin_controller.go
@@ -1,1 +1,50 @@
 package controllers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	models "github.com/Travelokay-Project/models"
+)
+
+func GetRefundList(w http.ResponseWriter, r *http.Request) {
+	db := Connect()
+	defer db.Close()
+
+	err := r.ParseForm()
+	if err != nil {
+		return
+	}
+
+	orderStatus := r.Form.Get("order_status")
+
+	rows, errQuery := db.Query("SELECT order_id,user_id,order_date,order_status,transaction_type FROM orders WHERE order_status=?", orderStatus)
+
+	var order models.Orders
+	var orders []models.Orders
+
+	for rows.Next() {
+		if err := rows.Scan(&order.ID, &order.UserID, &order.OrderDate, &order.OrderStatus, &order.TransactionType); err != nil {
+			log.Println(err.Error())
+		} else {
+			orders = append(orders, order)
+		}
+	}
+
+	var response models.OrdersResponse
+	if errQuery == nil {
+		if len(orders) == 0 {
+			SendErrorResponse(w, 400)
+		} else {
+			response.Status = 200
+			response.Message = "Success Get Data"
+			response.Data = orders
+		}
+	} else {
+		SendErrorResponse(w, 400)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+	db.Close()
+}
