efbdbc5c6208312a32a22ca3c03c18683d6f0452
adithiatirtaz@gmail.com <adithiatirtaz@gmail.com>
2022-04-18 11:41:39 +0700

    Add function new airplane

--- controllers/partner_controller.go
+++ controllers/partner_controller.go
@@ -11,68 +11,6 @@ import (
 	"github.com/Travelokay-Project/models"
 )
 
-func UpdatePartner(w http.ResponseWriter, r *http.Request) {
-
-	// Connect to database
-	db := Connect()
-	defer db.Close()
-
-	// Get value from cookie
-	partnerId := GetIdFromCookie(r)
-
-	// Get value from form
-	fullname := r.FormValue("fullname")
-	username := r.FormValue("username")
-	email := r.FormValue("email")
-	password := r.FormValue("password")
-	address := r.FormValue("address")
-	partnerType := r.FormValue("partnerType")
-	companyName := r.FormValue("companyName")
-
-	// encrypt password
-	hasher := md5.New()
-	hasher.Write([]byte(password))
-	encryptedPassword := hex.EncodeToString(hasher.Sum(nil))
-
-	// Query
-	query := "UPDATE users SET"
-
-	if fullname != "" {
-		query += " fullname = '" + fullname + "',"
-	}
-	if username != "" {
-		query += " username = '" + username + "',"
-	}
-	if email != "" {
-		query += " email = '" + email + "',"
-	}
-	if encryptedPassword != "" {
-		query += " password = '" + encryptedPassword + "',"
-	}
-	if address != "" {
-		query += " address = '" + address + "',"
-	}
-	if partnerType != "" {
-		query += " address = '" + partnerType + "',"
-	}
-	if companyName != "" {
-		query += " address = '" + companyName + "',"
-	}
-
-	queryNew := query[:len(query)-1] // Delete last coma
-	queryNew += " WHERE user_id = " + strconv.Itoa(partnerId)
-
-	_, errQuery := db.Exec(queryNew)
-
-	if errQuery != nil {
-		log.Println("(ERROR)\t", errQuery)
-		SendErrorResponse(w, 400)
-	} else {
-		SendSuccessResponse(w)
-		log.Println("(SUCCESS)\t", "Update partner request")
-	}
-}
-
 func GetFlightPartnerList(w http.ResponseWriter, r *http.Request) {
 
 	// Connect to database
@@ -200,6 +138,69 @@ func AddNewFlight(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+
+func UpdatePartner(w http.ResponseWriter, r *http.Request) {
+
+	// Connect to database
+	db := Connect()
+	defer db.Close()
+
+	// Get value from cookie
+	partnerId := GetIdFromCookie(r)
+
+	// Get value from form
+	fullname := r.FormValue("fullname")
+	username := r.FormValue("username")
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	address := r.FormValue("address")
+	partnerType := r.FormValue("partnerType")
+	companyName := r.FormValue("companyName")
+
+	// encrypt password
+	hasher := md5.New()
+	hasher.Write([]byte(password))
+	encryptedPassword := hex.EncodeToString(hasher.Sum(nil))
+
+	// Query
+	query := "UPDATE users SET"
+
+	if fullname != "" {
+		query += " fullname = '" + fullname + "',"
+	}
+	if username != "" {
+		query += " username = '" + username + "',"
+	}
+	if email != "" {
+		query += " email = '" + email + "',"
+	}
+	if encryptedPassword != "" {
+		query += " password = '" + encryptedPassword + "',"
+	}
+	if address != "" {
+		query += " address = '" + address + "',"
+	}
+	if partnerType != "" {
+		query += " address = '" + partnerType + "',"
+	}
+	if companyName != "" {
+		query += " address = '" + companyName + "',"
+	}
+
+	queryNew := query[:len(query)-1] // Delete last coma
+	queryNew += " WHERE user_id = " + strconv.Itoa(partnerId)
+
+	_, errQuery := db.Exec(queryNew)
+
+	if errQuery != nil {
+		log.Println("(ERROR)\t", errQuery)
+		SendErrorResponse(w, 400)
+	} else {
+		SendSuccessResponse(w)
+		log.Println("(SUCCESS)\t", "Update partner request")
+	}
+}
+
 func DeleteFlight(w http.ResponseWriter, r *http.Request) {
 
 	// Connect to database
@@ -258,4 +259,72 @@ func AddNewAirline(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+}
+
+func AddNewAirplane(w http.ResponseWriter, r *http.Request) {
+
+	// Connect to database
+	db := Connect()
+	defer db.Close()
+
+	// Get value from form
+	err := r.ParseForm()
+	if err != nil {
+		SendErrorResponse(w, 500)
+		log.Println("(ERROR)\t", err)
+		return
+	}
+
+	airlineId := r.Form.Get("airlineId")
+	airplaneModel := r.Form.Get("airplaneModel")
+
+	//Check if airline is already
+	if !CheckAirlineAlready(w, r, airlineId) {
+		log.Println("Data airplane tidak ada")
+		return
+	} else {
+
+		log.Println("Data airplane ada")
+	}
+
+	query := "INSERT INTO airplanes(airline_id, airplane_model) VALUES (?,?)"
+
+	_, errQuery := db.Exec(query, airlineId, airplaneModel)
+	if errQuery != nil {
+		log.Println("(ERROR)\t", errQuery)
+		SendErrorResponse(w, 400)
+		return
+	} else {
+		log.Println("(SUCCESS)\t", "Add new airline request")
+		SendSuccessResponse(w)
+		return
+	}
+
+}
+
+func CheckAirlineAlready(w http.ResponseWriter, r *http.Request, airlineId string) bool {
+
+	db := Connect()
+	defer db.Close()
+
+	// Query
+	rows, errQuery := db.Query("SELECT airline_id FROM airplanes WHERE airline_id = ?", airlineId)
+
+	if errQuery != nil {
+		log.Println("(ERROR)\t", errQuery)
+		SendErrorResponse(w, 500)
+		return false
+	}
+
+	var airline int
+
+	for rows.Next() {
+		if err := rows.Scan(&airline); err != nil {
+			log.Println("(ERROR)\t", err)
+			SendErrorResponse(w, 500)
+			return false
+		}
+	}
+
+	return true
 }
